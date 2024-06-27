ef06966fa52b0e08b7f44437e9c052733a955d1c
Shivans Awasthi <90861628+Shivans-Awasthi@users.noreply.github.com>
2024-06-27 09:48:35 +0530

    removed deploy

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -66,7 +66,7 @@ return(
       <br/><br/>
     </div>
     <div class="card-actions">
-      <a href="https://querylinker-web-app-querylinker.koyeb.app/"><button class="btn bg-blue-700 text-white ">Deployment</button></a>
+      {/* <a href="https://querylinker-web-app-querylinker.koyeb.app/"><button class="btn bg-blue-700 text-white ">Deployment</button></a> */}
       <a href="https://github.com/Shivans-Awasthi/QueryLinker"><button class="btn btn-dark text-white"><ion-icon name="logo-github"></ion-icon>Github</button></a>
     </div>
   </div>
