1a847d2ddbbcfae292b376366169d26b2daf80d8
Shivans Awasthi <90861628+Shivans-Awasthi@users.noreply.github.com>
2025-02-10 18:17:21 +0530

    UI changes

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -5,15 +5,14 @@ function Navigation(){
 
 return(
 
-<div class="navbar navbar-sm bg-black border-b border-gray-600 fixed z-20 h-`">
+<div class="navbar navbar-sm bg-black border-b border-gray-600 fixed z-20 h-1">
   <div class="navbar-start">
-  <div data-aos="fade-down" data-aos-duration="500"   data-aos-delay="0">
 
-    <div class="dropdown z-100">
+    {/* <div class="dropdown z-100">
       <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabindex="0" class="menu z-100 rounded-xl bg-neutral border border-white text-white menu-xl dropdown-content mt-3 p-2 shadow-xl rounded-box w-52">
+      <ul tabindex="0" class="menu z-100 rounded-xl bg-neutral border-white text-white menu-3xl dropdown-content mt-3 p-2 shadow-3xl rounded-box w-52">
         <li><a href="#intro">Intro</a></li>
         <li><a href="#skills">Skills</a></li>
         <li><a href="#education">Education</a></li>
@@ -23,8 +22,30 @@ return(
         <li><a href="#contact">Contact Me</a></li>
 
       </ul>
+    </div> */}
+
+    <div class="drawer">
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <label for="my-drawer" class="btn btn-ghost drawer-button lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      </div>
+      <div class="drawer-side">
+      <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+      <ul class="menu bg-neutral text-white min-h-full w-80 p-10">
+        <li><a href="#intro">Intro</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#education">Education</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#certificates">Certifications</a></li>
+        <li><a href="#blogs">Blogs</a></li>
+        <li><a href="#contact">Contact Me</a></li>
+      </ul>
+      </div>
     </div>
-    </div>
+
+
     <p class="text-lg ps-5 text-white ">Shivans</p>
     <p class="text-lg px-1 text-white ">Awasthi</p>
 
