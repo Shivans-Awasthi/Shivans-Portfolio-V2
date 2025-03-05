8ac840373f36cce6271967e32a092b6cb0b9d5a0
Shivans Awasthi <90861628+Shivans-Awasthi@users.noreply.github.com>
2025-03-05 13:06:06 +0530

    UI changes

--- src/app/components/Blogspost.jsx
+++ src/app/components/Blogspost.jsx
@@ -29,13 +29,13 @@ return(
 <div data-aos="zoom-in" data-aos-duration="500" data-aos-delay="200">
 
 <div data-aos="fade-down" data-aos-duration="1000" data-aos-delay="300">
-<div class="card glass1 rounded card-compact lg:w-80 sm:w-96 00 shadow-xl ">
+<div class="card glass1 rounded-none card-compact lg:w-80 sm:w-96 00 shadow-xl ">
   <figure><img src="https://www.hrkatha.com/wp-content/uploads/indian-flag-2644512_640.jpg" alt="OLQs" /></figure>
   
   <div class="card-body">
     <h2 class="card-title text-neutral-content text-black">Essential Requisite Qualities of an Officer</h2>
     <p class="text-black">Blogger</p>
-    <div class="card-actions justify-end">
+    <div class="card-actions justify-end"> 
       <a href="https://shivansawasthi.blogspot.com/2024/04/essential-requisite-qualities-of-officer.html"><button class="btn btn-outline bg-gray-200 text-black  hover:bg-black hover:text-white rounded-none">Open Blog</button></a>
     </div>
   </div>
