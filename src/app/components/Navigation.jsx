25492addda506e9f3d8859b49ad855d9772cb403
Shivans Awasthi <90861628+Shivans-Awasthi@users.noreply.github.com>
2025-03-07 20:07:50 +0530

    UI changes white cards

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -12,7 +12,7 @@ return(
     <input id="my-drawer" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content">
       <label for="my-drawer" class="btn btn-ghost drawer-button lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="black"><path stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="black"><path stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       </div>
       <div class="drawer-side">
