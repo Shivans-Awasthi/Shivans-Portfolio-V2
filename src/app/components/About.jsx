18c6cf274eac4cd69ac84c5ff0c2e616ad3b98ce
Shivans Awasthi <90861628+Shivans-Awasthi@users.noreply.github.com>
2025-03-24 23:18:18 +0530

    UI upgrade

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -7,6 +7,52 @@ return(
 <section id="about">
 
 
+<div class="hero bg-zinc-800 min-h-screen w-full">
+
+
+
+  <div class="hero-content w-full z-10 flex-col lg:flex-row md: mt-10">
+ 
+<div data-aos="fade-down" data-aos-duration="1000"  data-aos-delay="1000">
+    <div class="rounded-xl p-5 lg:p-10 bg-zinc-800">  
+    
+    <h1 class="text-5xl font-bold text-center text-white"><ion-icon name="person"></ion-icon> About Me</h1>
+
+<div class="bg-zinc-800 border-zinc-800 collapse border collapse-arrow">
+  <input type="checkbox" class="peer" />
+  <div class="collapse-title bg-zinc-800 text-white peer-checked:bg-secondary peer-checked:text-white">
+  <h1 class=" text-white text-left py-6 w-full">
+👋 Hello! I'm Shivans Awasthi, a passionate and dedicated Full Stack Software Engineer 🚀, specializing in the MERN Stack 💻, .NET Development 🏗️, and Data Science 📊. With a strong foundation in computer science and an insatiable curiosity for technology, I thrive on solving complex problems 🧩 and transforming innovative ideas into impactful solutions.
+</h1>
+  </div>
+  <div class="collapse-content  bg-zinc-800 text-white peer-checked:bg-secondary peer-checked:text-white">
+<p class="text-white">  
+From building scalable web applications 🌍 to crafting intelligent data-driven models 🧠, I love working on projects that challenge my technical prowess while delivering meaningful user experiences. My journey through engineering has been marked by a blend of creativity 🎨, persistence 🔑, and a relentless pursuit of excellence 🏅.
+<br/>
+Whether it's collaborating on high-performance software 🤝, contributing to robotics competitions 🤖, or mentoring aspiring developers 🎤, I believe in the power of technology to shape a better future.
+<br/>
+<p class="py-5 font-bold">What Drives Me?</p>
+
+📚 A desire to constantly learn and grow in the ever-evolving tech landscape.
+<br/>
+🛠️ Crafting solutions that merge engineering and creativity to solve real-world problems.
+<br/>
+🤝 Sharing knowledge and collaborating with like-minded individuals who are passionate about technology.
+<br/>
+<br/>
+When I'm not coding or brainstorming new ideas, you'll find me exploring innovative concepts 🏞️, engaging in robotics challenges 🤖, or writing blogs to inspire and inform 📝.
+<br/><br/>
+💬 Let's connect and bring your next project to life with enthusiasm and purpose! 🚀
+</p>
+  </div>
+</div>
+    
+
+    </div>
+    </div>
+  </div>
+  
+</div>
 
 
 </section>
